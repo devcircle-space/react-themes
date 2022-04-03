8e463e9c26494c8488916c1d790e0e269478019b
Aakash Jha <aakash.jha1103@gmail.com>
2022-04-03 16:48:12 +0530

    Styling for button and app

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ const App: React.FC = () => {
 	const { isDarkTheme, toggleTheme } = useContext(ThemeContext)
 
 	return (
-		<div className="app">
-			<button className="button" onClick={toggleTheme.bind(null)}>{`Switch to ${
+		<div className={`app ${isDarkTheme === true ? "dark" : ""}`}>
+			<button
+				className={`button ${isDarkTheme === true ? "dark" : ""}`}
+				onClick={toggleTheme.bind(null)}>{`Switch to ${
 				isDarkTheme === true ? "Light Theme" : "Dark Theme"
 			}`}</button>
 		</div>
